87bf2236ddcde6a44815cc1631c88ecf5d8f24c5
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-11-30 20:39:34 +0530

    welcome

---
+++ node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    internal class Node
+    {
+        public int data;
+        public Node next;
+
+        //constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
